0ff50a5ef2b6362fa5275fa4a4b02867548b54c3
dmmalawliya <dmmalawliya@gmail.com>
2024-11-18 14:45:27 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './fanta.css'
-import { AuthProvider } from './context/Authcontext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-    <App />
+      <App />
     </AuthProvider>
   </StrictMode>,
 )
