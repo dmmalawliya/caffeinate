19be27592fe48745265bfee949fea3bb9c3b9ca2
dmmalawliya <dmmalawliya@gmail.com>
2024-11-18 14:48:14 +0530

    added footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,19 @@
-
-import { useState } from "react"
+import { useState } from 'react'
 import Authentication from "./Authentication"
 import Modal from "./Modal"
-import { useAuth } from "../context/Authcontext"
-
+import { useAuth } from '../context/AuthContext'
 
 export default function Layout(props) {
     const { children } = props
 
-    const [showModal,setShowModal] = useState(false)
+    const [showModal, setShowModal] = useState(false)
 
     const { globalUser, logout } = useAuth()
 
     const header = (
         <header>
             <div>
-                <h1 className="text-gradient">CAFFEINATE</h1>
+                <h1 className="text-gradient">CAFFIEN8</h1>
                 <p>For Coffee Insatiates</p>
             </div>
             {globalUser ? (
@@ -28,21 +26,26 @@ export default function Layout(props) {
                     <i className="fa-solid fa-mug-hot"></i>
                 </button>
             )}
-            
         </header>
     )
 
     const footer = (
-        <footer>
-            <p><span className="text-gradient">Track your coffee journey using Caffeinate</span></p>
+        <footer className='footer-container'>
+             <p><span className="text-gradient">Caffienate</span> was made by <a target="_blank" href="https://www.linkedin.com/in/deepshikha-malawliya-447176206/">Deepshikha</a> <br />Check out the project on <a target="_black" href="https://github.com/dmmalawliya/caffeinate">GitHub</a>!</p>
         </footer>
     )
 
+    function handleCloseModal() {
+        setShowModal(false)
+    }
+
     return (
         <>
-            {showModal && (<Modal handleCloseModal ={() => { setShowModal(false)}}>
-                <Authentication/>
-            </Modal>)}
+            {showModal && (
+                <Modal handleCloseModal={handleCloseModal}>
+                    <Authentication handleCloseModal={handleCloseModal} />
+                </Modal>
+            )}
             {header}
             <main>
                 {children}
